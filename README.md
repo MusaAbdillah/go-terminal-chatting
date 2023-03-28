08cf6f1a63b612774b49e4a141f12272fcd86130
Musa Abdillah <musa.abdillah@ottodigital.id>
2023-03-28 10:31:18 +0700

    intialize chatting with ai machine in go

---
+++ README.md
@@ -0,0 +1,5 @@
+# How to run
+
+- Set you api key on .env file
+- Run go run main.go
+- You are done, happing chatting with ai machine!
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+	openai "github.com/sashabaranov/go-openai"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	var content string
+	client := openai.NewClient(os.Getenv("OPEN_AI_API_KEY"))
+
+	for {
+
+		content = StringPrompt("Musa: ")
+
+		resp, err := client.CreateChatCompletion(
+			context.Background(),
+			openai.ChatCompletionRequest{
+				Model: openai.GPT3Dot5Turbo,
+				Messages: []openai.ChatCompletionMessage{
+					{
+						Role:    openai.ChatMessageRoleUser,
+						Content: content,
+					},
+				},
+			},
+		)
+
+		if err != nil {
+			fmt.Printf("ChatCompletion error: %v\n", err)
+			return
+		}
+
+		fmt.Println(resp.Choices[0].Message.Content)
+	}
+
+}
+
+func StringPrompt(label string) string {
+	var s string
+	r := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Fprint(os.Stderr, label+" ")
+		s, _ = r.ReadString('\n')
+		if s != "" {
+			break
+		}
+	}
+	return strings.TrimSpace(s)
+}
